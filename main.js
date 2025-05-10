429ccb1b4ae3775779cf77acca7b60abb0da1520
SemVR <sem.wlad26@gmail.com>
2025-05-10 15:45:50 +0300

    compl

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const imgsSrc = [
 	'https://picsum.photos/id/82/200/300',
 ]
 
+const singlImgSize = 200
 const body =  document.getElementsByTagName('body')[0]
 
 function createMainConteiner (){
@@ -22,7 +23,6 @@ function createImgElement (src){
 	return imgElement
 }
 
-
 function getNextPositionCreator(numberOfSlide){
 	let currentSlide = 0
 	return function (directionForvard = true){
@@ -32,7 +32,7 @@ function getNextPositionCreator(numberOfSlide){
 				currentSlide = 0
 			}
 		}else{
-		--currentSlide
+			--currentSlide
 		if(currentSlide<0){
 			currentSlide = currentSlide -1
 			}
@@ -41,22 +41,20 @@ function getNextPositionCreator(numberOfSlide){
 	}
 }
 
-// function createMainImg(src, conteiner){
-// 	const img = createImgElement(src)
-// 	conteiner.appendChild(img)
-// 	return img
-// }
-
 function handSlideChange(nextSlidePosition){
-	const allImgs = Array.from(document.getElementsByTagName('img'))
-	allImgs.forEach(img=>img.style.display = 'none')
-	allImgs[nextSlidePosition].style.display = 'block'
+	const firstImg = document.querySelector("img[src='https://picsum.photos/id/22/200/300']");
+	firstImg.style['margin-left'] = `${nextSlidePosition * - singlImgSize}px`
+	const currentIndicator =  document.getElementsByClassName('slide-indicator-active')[0]
+	currentIndicator.classList.toggle('slide-indicator-active')
+
+	const indicators =  document.getElementsByClassName('slide-indicator')
+	indicators[nextSlidePosition].classList.toggle('slide-indicator-active')
+	
 }
 
 function createAndRenderImages (imgsSrc, conteiner){
 	const listOfImageElements = imgsSrc.map(createImgElement)
 	listOfImageElements.forEach((imgElement) => conteiner.appendChild(imgElement))
-	handSlideChange(0)
 }
 
 function createCarouselButtons(getNextSlide, mainImgElement){
@@ -74,17 +72,99 @@ function createCarouselButtons(getNextSlide, mainImgElement){
 		const slide = getNextSlide(false)
 		handSlideChange(slide)
 	})
+	const buttonConteiner = document.createElement('div')
+	buttonConteiner.classList.toggle('buttons-conteiner')
+	body.appendChild(buttonConteiner)
 
-	body.appendChild(leftButton)
-	body.appendChild(righrButton)
+	buttonConteiner.appendChild(leftButton)
+	buttonConteiner.appendChild(righrButton)
 }
 
 
-const main = createMainConteiner();
-createAndRenderImages(imgsSrc, main)
-const getNextSlide = getNextPositionCreator(imgsSrc.length)
+function addPlayPauseButton(){
+	const button = document.createElement('button')
+	button.innerText = 'Pause'
+	body.appendChild(button)
+	let autoplayState = true
+	button.addEventListener('click', ()=>{
+		button.innerText = autoplayState ? 'Play' : 'Pause'
+		autoplayState = !autoplayState
+})
+	return button
+}
 
-//createMainImg(imgsSrc[0], main);
+function createSlidesIndicators(length){
+	const conteiner = document.createElement('div')
+	conteiner.classList.toggle('slide-indicator-conteiner')
+	for(let i = 0; i<length; i++){
+		let indicator = document.createElement('div')
+		indicator.classList.toggle('slide-indicator')
+		if(i === 0)(indicator.classList.toggle('slide-indicator-active'))
+		conteiner.appendChild(indicator)
+	}
+	body.appendChild(conteiner)
+}
+
+function addSwipes(element){
+	let touchstartX = 0
+	let touchendX = 0
+
+	function checkDirection(){
+		if(touchendX > touchstartX) handSlideChange(getNextSlide(false))
+		if(touchendX < touchstartX) handSlideChange(getNextSlide())
+	}
+
+	element.addEventListener('touchstart', e =>{
+		touchstartX = e.changedTouches[0].screenX
+	})
+
+	element.addEventListener('touchend', e =>{
+		touchendX = e.changedTouches[0].screenX
+		checkDirection()
+	})
+}
+
+function addSwipesMouse(element){
+	let touchstartX = 0
+	let touchendX = 0
+
+	function checkDirection(){
+		if(touchendX > touchstartX) handSlideChange(getNextSlide(false))
+		if(touchendX < touchstartX) handSlideChange(getNextSlide())
+	}
+
+	element.addEventListener('mousedown', e =>{
+		touchstartX = e.x
+	})
+
+	element.addEventListener('mouseup', e =>{
+		touchendX = e.x
+		checkDirection()
+	})
+}
+
+const main = createMainConteiner();
+createAndRenderImages(imgsSrc, main);
+
+const getNextSlide = getNextPositionCreator(imgsSrc.length);
 createCarouselButtons(getNextSlide);
 
+const playPauseButton = addPlayPauseButton()
+
+createSlidesIndicators(imgsSrc.length)
+
+setInterval(() =>{
+	if(playPauseButton.innerText === 'Pause'){
+		const slide = getNextSlide()
+		handSlideChange(slide)
+	}
+}, 1500)
+
+document.addEventListener('keydown', (event)=>{
+	if(event.key === 'ArrowLeft') handSlideChange(getNextSlide(false))
+	if(event.key === 'ArrowRight') handSlideChange(getNextSlide())
+})
+
+addSwipes(document)
+addSwipesMouse(document)
 
